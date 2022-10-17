29903f84c19be0191633363f3909646fc4184d55
CrispyCodeLegend <Ibrahimamustafa35@gmail.com>
2022-10-16 21:21:25 -0400

    update

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #Python Setup Link https://code.visualstudio.com/docs/python/python-tutorial
 import os
 import openai
+
+qString = "";
+
+
 openai.api_key = "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"
 searchRequest ="Give me a list of Hotels in Cabo San Lucas, Mexico that cost less than $1000 per day and have a 4 star rating"
 response = openai.Completion.create(
@@ -9,4 +13,7 @@ response = openai.Completion.create(
   max_tokens=256,
   temperature=0.7
 )
+
+input("Enter your name:")
+
 print(response)

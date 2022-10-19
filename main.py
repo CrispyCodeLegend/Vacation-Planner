47f79cd26d3179e4b993e2ff3059287a4e29a146
CrispyCodeLegend <Ibrahimamustafa35@gmail.com>
2022-10-18 21:16:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,38 @@ import os
 from tkinter.messagebox import YES
 import openai
 
+# Initial message to explain the application and how to use it
+print("This application will take your answers and randomly pull locations that match what you wanted.")
 print("For the best results when answering questions, follow the examples shown at the end of each message.")
 
+# Initialize Query String
+# Will be sent to GPT-3
 qString = "Give me a list of "
 
+# input() method prints whatever string you have in the parenthesis
+# and returns whatever input was provided in the terminal
+# Also storing the location type input to be used later in the code 
 location = input(
     "What type of location would you like to stay at?(E.G. hotel): ")
+
+# The repeated qString += is concatenating the input from the user to the end of the Query String
 qString += location + "s in "
 qString += input("Where do you want to go? (E.G. city, state or state, country): ")
+
+# GPT-3 seems to use daily price of staying for one person, 
+# so the program requests the amount of days the user will be staying,
+# the amount of people they are paying for, and their budget.
 days = float(input("How many days do you wish to stay? (E.G. 1,2,3,etc.): "))
 people = float(
     input("How many people are you paying for? (E.G. 1,2,3,etc.): "))
-cost = float(input("What is your budget?(E.G. 1000): "))
+cost = float(input("What is your budget in US dollars?(E.G. 1000): "))
+
+# perDayCost is the max price the user can spend for each person every day and stay in budget
 perDayCost = str(round(cost/people/days, 2))
 qString += " that cost less than $" + perDayCost + " per day and have a "
 qString += input("What rating(stars specifically) would you like the establishment to have?(E.G. 1,2,3-5,etc.): ") + " star rating."
 
+# Optional Questions below that can be answered with a yes or no response. 
 if input("Would you like to answer additional questions to make the search more accurate?(E.G. yes or no): ") == "yes":
     print("For the following questions, answer with yes or no.")
     questions = ["Wifi", "Breakfast", "Cable",
@@ -45,7 +61,7 @@ if input("Would you like to answer additional questions to make the search more 
 
 print(qString)
 
-openai.api_key = "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"
+openai.api_key = "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"
 searchRequest = qString
 response = openai.Completion.create(
     model="text-davinci-002",
